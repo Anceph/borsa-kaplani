ba21fc61bccb41a13040def06a5cb67c91d9ed22
Anceph <41387237+Anceph@users.noreply.github.com>
2023-11-21 15:49:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# v14-slash-template
+discord.js v14 es6 bot template with interactions
---
+++ src/commands/normal/test.js
@@ -0,0 +1,8 @@
+export default {
+    name: "test",
+    aliases: [],
+    cooldown: 5000,
+    run: async (client, message, args) => {
+        message.reply('pong')
+    }
+};
